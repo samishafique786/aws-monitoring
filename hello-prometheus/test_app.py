9b91285c7cd0fdc56d19ed9813ec6bfd1864ce4d
Sami Shafique <samishafique786@gmail.com>
2025-11-30 21:34:56 +0200

    version 2

--- hello-prometheus/test_app.py
+++ hello-prometheus/test_app.py
@@ -11,7 +11,7 @@ def test_hello_endpoint(client):
     """Test that the / endpoint returns 'hello world'"""
     response = client.get("/")
     assert response.status_code == 200
-    assert response.data == b"hello world sami"
+    assert response.data == b"hello world version 2"
 
 def test_metrics_endpoint(client):
     """Check /metrics output"""
