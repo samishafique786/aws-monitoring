3727dba220f3e27bf7f83352bce5740d2b3ea0fd
Sami Shafique <samishafique786@gmail.com>
2025-11-30 23:15:48 +0200

    hello world

--- hello-prometheus/app.py
+++ hello-prometheus/app.py
@@ -20,7 +20,7 @@ def hello_world():
     REQUEST_COUNT.inc()
     
     # Simulate processing (optional)
-    response_text = "hello world version 2"
+    response_text = "hello world"
     
     elapsed = time.time() - start_time
     REQUEST_LATENCY.observe(elapsed)
